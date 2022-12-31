cf628d676237abca6031da1d7d363f453f895a7c
priceymineral <tudaeva@gmail.com>
2022-12-30 16:36:49 -0800

    added header components

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import githubLogo from '../assets/mark-github.svg';
+import liLogo from '../assets/linked-in.png'
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <div className="name">
+                Octavio Sifuentes
+            </div>
+            <div className="links-container">
+                <a  className="github" href="https://github.com/priceymineral" target="_blank" rel="noreferrer">
+                    <img src={githubLogo} alt="github logo" />
+                </a>
+                <a  className="linked-in" href="https://www.linkedin.com/in/octavio-sifuentes/" target="_blank" rel="noreferrer">
+                    <img src={liLogo} alt="linkedIn logo" />
+                </a>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header

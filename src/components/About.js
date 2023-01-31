1a467b970454693d1bbbfd0420e18b2040e7a270
priceymineral <tudaeva@gmail.com>
2023-01-30 19:35:52 -0800

    changed About

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
         <div className="about-header">About</div>
         <div className="about-container">
             <p className="about">
-                I am a software developer transitioning from nuclear power plant operations in the navy. <br></br> I am a student at LEARN Academy where I am enjoying meeting and collaborating with a diverse group of students <br></br> as well learning the tools that will help me bring my app ideas to fruition.
+                I am a software developer who transitioned from nuclear power plant operations in the navy. <br></br> I am a student at LEARN Academy where I am enjoying meeting and collaborating with a diverse group of students <br></br> as well learning the tools that will help me bring my app ideas to fruition.
             </p>
         </div>
         </>

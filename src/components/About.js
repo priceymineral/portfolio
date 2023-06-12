9b028e2ba1e85e2117ea5dee8748783f1cd5a6c2
priceymineral <tudaeva@gmail.com>
2023-06-12 16:23:37 -0700

    Updated about me to include topdev

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
             <div className="subheader">About</div>
             <div className="about-container">
                 <p className="about">
-                    I am a software developer with a BS in Physics who transitioned from a career in nuclear power plant operations in the US Navy. <br></br> My latest experience includes working at MightyNest, where I optimized the code base to minimize dependencies,<br></br> increase maintainability/development speed and implemented WCAG changes to improve accessibility.<br></br>I would <a href="https://en.wikipedia.org/wiki/Love" target="_blank" rel="noreferrer">💖</a> to hear about what you do and see how our interests might align, please don't hesitate to reach out (<span
+                    I am a software developer with a BS in Physics who transitioned from a career in nuclear power plant operations in the US Navy. <br></br>I am currently a jr back-end developer intern at TopDev.<br></br>I would <a href="https://en.wikipedia.org/wiki/Love" target="_blank" rel="noreferrer">💖</a> to hear about what you do and see how our interests might align, please don't hesitate to reach out (<span
                         style={{
                             color: "white",
                             fontSize: "20px",
